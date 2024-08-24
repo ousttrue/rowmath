3f652457c4f3f235fca108c43753f702ff3dd7b0
ousttrue <ousttrue@gmail.com>
2024-08-25 03:58:27 +0900

    fix screenshot

--- docs/screenshot.ts
+++ docs/screenshot.ts
@@ -2,18 +2,23 @@ const list = require('./src/components/HomepageFeatures/list.json');
 const pw = require('playwright');
 
 (async () => {
-  const browser = await pw.chromium.launch(); // or 'chromium', 'firefox'
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  page.setViewportSize({ "width": 300, "height": 157 });
 
-  for(const item of list){
+  for (const item of list) {
+    console.log(`${item.name} (${item.base_name})`);
+    try {
+      const browser = await pw.chromium.launch(); // or 'chromium', 'firefox'
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      page.setViewportSize({ "width": 300, "height": 157 });
 
-    await page.goto(`http://localhost:3000/rowmath/wasm/${item.base_name}.html`);
-    await page.waitForLoadState('networkidle')
-    await page.screenshot({ path: `static/wasm/${item.base_name}.jpg` });
+      await page.goto(`http://localhost:3000/rowmath/wasm/${item.base_name}.html`);
+      await page.waitForLoadState('networkidle')
+      await page.screenshot({ path: `static/wasm/${item.base_name}.jpg` });
 
+      await browser.close();
+    } catch (ex) {
+      console.error(ex);
+    }
   }
 
-  await browser.close();
 })();
