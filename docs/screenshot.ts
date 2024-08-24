5d0de6dbe27ca7334d60dd076cc62fc0b5cd719e
ousttrue <ousttrue@gmail.com>
2024-08-25 04:08:06 +0900

    catch

--- docs/screenshot.ts
+++ docs/screenshot.ts
@@ -5,20 +5,18 @@ const pw = require('playwright');
 
   for (const item of list) {
     console.log(`${item.name} (${item.base_name})`);
+    const browser = await pw.chromium.launch(); // or 'chromium', 'firefox'
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    page.setViewportSize({ "width": 300, "height": 157 });
     try {
-      const browser = await pw.chromium.launch(); // or 'chromium', 'firefox'
-      const context = await browser.newContext();
-      const page = await context.newPage();
-      page.setViewportSize({ "width": 300, "height": 157 });
-
       await page.goto(`http://localhost:3000/rowmath/wasm/${item.base_name}.html`);
       await page.waitForLoadState('networkidle')
       await page.screenshot({ path: `static/wasm/${item.base_name}.jpg` });
-
-      await browser.close();
     } catch (ex) {
       console.error(ex);
     }
+    await browser.close();
   }
 
 })();
