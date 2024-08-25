4621e985ace55ab0f24318c397ba48e729a6d409
ousttrue <ousttrue@gmail.com>
2024-08-26 02:33:03 +0900

    bvh Tokenizer

--- src/bvh/Tokenizer.zig
+++ src/bvh/Tokenizer.zig
@@ -6,17 +6,32 @@ const Result = struct {
 };
 const Delimiter = fn ([]const u8) ?usize;
 
-fn is_space(_it: []const u8) ?usize {
-    if (!std.ascii.isWhitespace(_it[0])) {
+fn is_space(str: []const u8) ?usize {
+    if (!std.ascii.isWhitespace(str[0])) {
         return null;
     }
 
-    for (_it, 0..) |c, i| {
+    for (str, 0..) |c, i| {
         if (!std.ascii.isWhitespace(c)) {
             return i;
         }
     }
-    return _it.len;
+
+    return str.len;
+}
+
+fn get_name(str: []const u8) ?usize {
+    if (str[0] != '\n') {
+        return null;
+    }
+    // head space
+    for (str, 0..) |c, i| {
+        if (!std.ascii.isWhitespace(c)) {
+            return i;
+        }
+    }
+
+    return str.len;
 }
 
 data: []const u8,
@@ -37,7 +52,9 @@ pub fn token(self: *@This(), delimiter: *const Delimiter) ?[]const u8 {
             return self.remain[0..i];
         }
     }
-    return null;
+
+    defer self.remain = self.remain[self.remain.len..self.remain.len];
+    return self.remain;
 }
 
 pub fn expect(self: *@This(), expected: []const u8, delimiter: *const Delimiter) bool {
@@ -49,22 +66,20 @@ pub fn expect(self: *@This(), expected: []const u8, delimiter: *const Delimiter)
     return false;
 }
 
-// template<typename T>
-// std::optional<T> number(const Delimiter& delimiter)
-// {
-//   auto n = token(delimiter);
-//   if (!n) {
-//     return {};
-//   }
-//   if (auto value = to_num<T>(*n)) {
-//     return *value;
-//   } else {
-//     return {};
-//   }
-// }
-//
+pub fn number(self: *@This(), T: type, delimiter: *const Delimiter) ?T {
+    const n = self.token(delimiter) orelse {
+        return null;
+    };
+
+    return switch (T) {
+        f32 => std.fmt.parseFloat(T, n) catch null,
+        i32 => std.fmt.parseInt(T, n, 10) catch null,
+        else => unreachable,
+    };
+}
 
 test {
+    // https://qiita.com/matchyy/items/ee99fb28110e4614323f
     const src =
         \\HIERARCHY
         \\ROOT Hips
@@ -83,7 +98,45 @@ test {
         \\}
     ;
 
-    var tokenizer = Tokenizer.init(src);
+    var t = Tokenizer.init(src);
 
-    try std.testing.expect(tokenizer.expect("HIERARCHY", &is_space));
+    try std.testing.expect(t.expect("HIERARCHY", &is_space));
+    try std.testing.expect(t.expect("ROOT", &is_space));
+    try std.testing.expect(t.expect("Hips", &get_name));
+    try std.testing.expect(t.expect("{", &is_space));
+    try std.testing.expect(t.expect("OFFSET", &is_space));
+    try std.testing.expectEqual(0, t.number(f32, &is_space).?);
+    try std.testing.expectEqual(0, t.number(f32, &is_space).?);
+    try std.testing.expectEqual(0, t.number(f32, &is_space).?);
+    try std.testing.expect(t.expect("CHANNELS", &is_space));
+    try std.testing.expectEqual(6, t.number(i32, &is_space).?);
+    try std.testing.expect(t.expect("Xposition", &is_space));
+    try std.testing.expect(t.expect("Yposition", &is_space));
+    try std.testing.expect(t.expect("Zposition", &is_space));
+    try std.testing.expect(t.expect("Yrotation", &is_space));
+    try std.testing.expect(t.expect("Xrotation", &is_space));
+    try std.testing.expect(t.expect("Zrotation", &is_space));
+    try std.testing.expect(t.expect("JOINT", &is_space));
+    try std.testing.expect(t.expect("Chest", &get_name));
+    try std.testing.expect(t.expect("{", &is_space));
+    try std.testing.expect(t.expect("OFFSET", &is_space));
+    try std.testing.expectEqual(0, t.number(f32, &is_space).?);
+    try std.testing.expectEqual(10.678932, t.number(f32, &is_space).?);
+    try std.testing.expectEqual(0.0062800, t.number(f32, &is_space).?);
+    try std.testing.expect(t.expect("CHANNELS", &is_space));
+    try std.testing.expectEqual(3, t.number(i32, &is_space).?);
+    try std.testing.expect(t.expect("Yrotation", &is_space));
+    try std.testing.expect(t.expect("Xrotation", &is_space));
+    try std.testing.expect(t.expect("Zrotation", &is_space));
+    try std.testing.expect(t.expect("End", &is_space));
+    try std.testing.expect(t.expect("Site", &get_name));
+    try std.testing.expect(t.expect("{", &is_space));
+    try std.testing.expect(t.expect("OFFSET", &is_space));
+    try std.testing.expectEqual(0, t.number(f32, &is_space).?);
+    try std.testing.expectEqual(16.966594, t.number(f32, &is_space).?);
+    try std.testing.expectEqual(-0.014170, t.number(f32, &is_space).?);
+    try std.testing.expect(t.expect("}", &is_space));
+    try std.testing.expect(t.expect("}", &is_space));
+    try std.testing.expect(t.expect("}", &is_space));
+    try std.testing.expectEqual(0, t.remain.len);
 }
